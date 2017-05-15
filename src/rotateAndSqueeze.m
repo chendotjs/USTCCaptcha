501e94a648fada9cf198abd671ecdbc848935c6a
chendotjs <chendotjs@qq.com>
2017-05-15 19:39:30 +0800

    finish rotate

--- src/rotateAndSqueeze.m
+++ src/rotateAndSqueeze.m
@@ -4,10 +4,34 @@ function [rAngle, Ir] = rotateAndSqueeze(I, pointX, pointY, angle1, angle2)
     Ir = imrotatep(I, pointX, pointY, angle);
     % figure();
     % imshow(Ir);
-    width = [width, squeeze(Ir)]
+    width = [width, squeeze(Ir)];
   end
-  plot(angle1:angle2, width, 'ko');
-  % TODO: 找到极小值的点然后旋转图像
+  % 找到极小值的点然后旋转图像
+  minwidth = min(width);
+  logicIdx = width == ones(size(width)) * minwidth; % 为1，预示着哪些角度是最瘦的旋转角度
+  % 下面就是找到最长的一段为1的连续子序列，去中点作为旋转角度
+  flag = zeros(length(logicIdx), length(logicIdx));
+  maxLength = -1; % 最长连续子序列的长度
+  i = 1; j = i;
+  while i <= length(logicIdx)
+    if logicIdx(i) == 1
+      j = i;
+      while j <= length(logicIdx) && logicIdx(j) == 1
+        j = j + 1;
+      end
+      flag(i, j) = j - i;
+      if j - i > maxLength
+        maxLength = j - i;
+        left = i;
+        right = j;
+      end
+      i = j;
+    else
+      i = i + 1;
+    end
+  end
+  rAngle = (left + right) / 2 + angle1;
+  Ir = imrotatep(I, pointX, pointY, rAngle);
 end  % function
 
 
@@ -15,14 +39,14 @@ function width = squeeze(I)
   [x, y] = size(I);
 
   for j = 1:y
-    if sum(I(:,j)) < 255 * x
+    if prod(I(:,j)) == 0
       left = j;
       break;
     end
   end
 
   for j = sort(1:y,'descend')
-    if sum(I(:,j)) < 255 * x
+    if prod(I(:,j)) == 0
       right = j;
       break;
     end
