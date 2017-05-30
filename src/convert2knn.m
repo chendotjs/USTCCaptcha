bf3ada2259175217cbf020903655300658036fdf
chendotjs <chendotjs@gmail.com>
2017-05-30 19:46:04 +0800

    fix knn data bug

--- src/convert2knn.m
+++ src/convert2knn.m
@@ -4,11 +4,13 @@ load trainData.mat;
 knnChars = [];
 knnLabels = [];
 for i = 1:length(chars)/25
-  vec = reshape(chars(:, 25 * i: 25 * i + 24), 1, 625);
+  i
+  vec = reshape(chars(:, 25 * (i-1) + 1: 25 * i), 1, 625);
   knnChars = [knnChars;vec];
 end
 
 for i = 1:length(labels)
+  i
   knnLabels = [knnLabels;labels(i)];
 end
 
